d96b096ca386db8e14ae499ad77d7a6d31158ae6
Ritwik Jha <64138168+ritwik15416@users.noreply.github.com>
2020-08-31 17:16:32 +0530

    Update dictionary.py

--- Part-1/Dictionary/dictionary.py
+++ Part-1/Dictionary/dictionary.py
@@ -1,1 +1,20 @@
-sdas
+import json
+def dic():
+    print('Welcome to the dictionary!')
+    print('If you want to quit, type EXIT')
+    data = json.load(open("data.json"))
+    flag = True
+    while(flag):
+        s = input('What do you want to search: ')
+        if s=='EXIT':
+            flag = False
+        else:
+            search = s.lower()
+            if search in data:
+                print(data[search])
+            else:
+                print('Sorry, word not found in dictionary')
+                print('Check for spelling errors and try again.')
+        
+dic()
+        
